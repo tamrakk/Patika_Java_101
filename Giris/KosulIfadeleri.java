8316221c45722e0c3b1d552e9008ac0f474bac50
Bekir Tamrak <tamrakbekir@gmail.com>
2024-02-13 20:22:32 +0300

    update5

---
+++ Giris/KosulIfadeleri.java
@@ -0,0 +1,25 @@
+package Giris;
+
+public class KosulIfadeleri {
+    public static void main(String[] args) {
+
+        int a=10, b = 20, c = 1;
+
+
+        if ((a<b) && (a<c)){
+
+            System.out.println("A en küçük sayıdır");
+
+        } else if ((b<a) && (b<c)) {
+            System.out.println("B en küçük sayıdır");
+
+
+        }else{
+            System.out.println(" C en küçük sayıdır");
+        }
+
+    }
+
+
+
+}
