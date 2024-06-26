403add3cd9f3563249768a6b42d4e36a53291f3f
Bekir Tamrak <tamrakbekir@gmail.com>
2024-06-26 15:44:25 +0300

    update5

---
+++ donguler/Cift_sayi.java
@@ -0,0 +1,23 @@
+package donguler;
+import java.util.Scanner;
+
+public class Cift_sayi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k;
+
+        System.out.println("bir sayı giriniz");
+        k=input.nextInt();
+        for (int i= 1; i<=k; i++){
+            if (i%2 ==0){
+                System.out.println(i);
+            }
+        }
+
+
+
+
+
+
+    }
+}
